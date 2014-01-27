d69c74e8cd6fc536a5a4da3209b2b1db3de36a38
F03 H4MM3R <F03 H4MM3R@10.30.16.236>
2014-01-27 13:55:52 -0600

    moved code in controller to doPost method, was in the wrong one

--- src/java/controller/AreaController.java
+++ src/java/controller/AreaController.java
@@ -24,44 +24,10 @@ import model.RectangleCalculations;
 public class AreaController extends HttpServlet {
     private static final String RESULT_PAGE = "result.jsp";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String w = request.getParameter("rectagleWidth");
-        double wt = Double.parseDouble(w);
-        String h = request.getParameter("rectagleHeight");
-        double ht = Double.parseDouble(h);
-        RectangleCalculations rc = new RectangleCalculations();
-        double result = rc.getArea(wt,ht);
-        
-        request.setAttribute("recommendations", result);
-        
-        RequestDispatcher view =
-                request.getRequestDispatcher(RESULT_PAGE);
-        view.forward(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
@@ -75,7 +41,20 @@ public class AreaController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html");
+        
+        String w = request.getParameter("rectagleWidth");
+        double wt = Double.parseDouble(w);
+        String h = request.getParameter("rectagleHeight");
+        double ht = Double.parseDouble(h);
+        RectangleCalculations rc = new RectangleCalculations();
+        double result = rc.getArea(wt,ht);
+        
+        request.setAttribute("recommendations", result);
+        
+        RequestDispatcher view =
+                request.getRequestDispatcher(RESULT_PAGE);
+        view.forward(request, response);
     }
 
     /**
